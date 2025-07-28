38f87c633891c36d9a9ddf5e115e3f8b9ba20385
bilalaurang <bilalaurangzeb001@gmail.com>
2025-07-27 23:43:41 -0700

    Fixed Car model class not found error

--- app/Models/Car.php
+++ app/Models/Car.php
@@ -2,16 +2,14 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Car extends Model
 {
-    use HasFactory;
-
     protected $fillable = [
         'make', 'model', 'year', 'price', 'kilometers', 'color', 'body_condition',
-        'mechanical_condition', 'engine_size', 'horsepower', 'top_speed', 'steering_side',
-        'wheel_size', 'interior_color', 'seats', 'doors', 'description', 'showroom_info'
+        'mechanical_condition', 'engine_size', 'horsepower', 'top_speed',
+        'steering_side', 'wheel_size', 'interior_color', 'seats', 'doors',
+        'showroom_info', 'description'
     ];
 }
